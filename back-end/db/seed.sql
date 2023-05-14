8f0e02129a6d8b95d4517183b57b65e42fe7d245
i-guerrero <isaacguerrero@pursuit.org>
2023-05-14 08:30:22 -0400

    fix merge main into project_routes

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -7,8 +7,6 @@ VALUES
 INSERT INTO proposals (title, description, impact, status, non_profit_id, mentor_id)
 VALUES('Family Friendly Website', 'I am highly interested in creating a Website that will cater to all of the needs for low-income families. My non-profit focuses on providing resources like information on programs for qualifying low-income families as well as programs we have created ourselves like our annual food drive. I would like an all-in-one inclusive website that the families can use to learn about these programs and other resources.', 'I believe the financial impact this will provide is somewhere between $5,000 & $10,000', 'pending', 1, 1);
 
-('Bob', 'Milton', 'FamilyFlourish@gmail.com', 'Family Flourish Fund', 'Omaha', 'United States', 'Bob_Family_flourish', 'password', 'nonprofit', 'www.linkedin.com/in/Bob_Milton');
-
 INSERT INTO projects (technologies, num_developers, time_to_complete, mentor_id, tasks, status)
 VALUES 
 ('JavaScript, React, Express', 3, '6 months', 1, 'build basic express back end, create react app, nav bar, sign up form, build routes', 'pending');
